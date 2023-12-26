bbe20e1f06468fd8a886d0301e98fa9650800fb1
YS <35719286+YS1109@users.noreply.github.com>
2023-12-26 15:32:24 +0800

    通过JNA获取进程是32位还是64位的

--- wechat-analyze/src/main/java/com/ysoztf/wechat/analyze/wxinfo/impl/WxInfoServiceImpl.java
+++ wechat-analyze/src/main/java/com/ysoztf/wechat/analyze/wxinfo/impl/WxInfoServiceImpl.java
@@ -1,5 +1,10 @@
 package com.ysoztf.wechat.analyze.wxinfo.impl;
 
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Tlhelp32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.ptr.IntByReference;
 import com.ysoztf.wechat.analyze.wxinfo.WxInfoService;
 import com.ysoztf.wechat.analyze.wxinfo.beans.WxInfoBean;
 
@@ -68,9 +73,39 @@ public class WxInfoServiceImpl implements WxInfoService {
         return fileBasePath;
     }
 
+    private boolean getIsWow64(int pid) {
+        boolean isWow64 = false;
+        // Windows 下的动态链接库，允许访问 Windows API
+        Kernel32 kernel32 = Kernel32.INSTANCE;
+
+        // 创建了一个进程快照，用于获取当前系统中运行的进程列表
+        WinNT.HANDLE snapshot = kernel32.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPPROCESS, new WinDef.DWORD(0));
+        Tlhelp32.PROCESSENTRY32.ByReference processEntry = new Tlhelp32.PROCESSENTRY32.ByReference();
+        try {
+            while (kernel32.Process32Next(snapshot, processEntry)) {
+                // 通过遍历进程列表，找到特定 PID 对应的进程
+                if (processEntry.th32ProcessID.intValue() == pid) {
+                    // 使用 OpenProcess 打开该进程以获取它的处理权限
+                    WinNT.HANDLE process = kernel32.OpenProcess(Kernel32.PROCESS_QUERY_INFORMATION, false, pid);
+                    IntByReference wow64Process = new IntByReference(0);
+                    // IsWow64Process 是用于确定指定进程是否为32位或64位的方法，它将返回一个标志，指示进程是32位还是64位
+                    kernel32.IsWow64Process(process, wow64Process);
+                    if (wow64Process.getValue() == 0) {
+                        isWow64 = true;
+                    }
+                    break;
+                }
+            }
+        } finally {
+            kernel32.CloseHandle(snapshot);
+        }
+        return isWow64;
+    }
+
     public static void main(String[] args) {
         WxInfoServiceImpl wxInfoService = new WxInfoServiceImpl();
         System.out.println(wxInfoService.getWechatPid());
         System.out.println(wxInfoService.getWechatFileBasePath());
+        System.out.println(wxInfoService.getIsWow64(wxInfoService.getWechatPid()));
     }
 }
